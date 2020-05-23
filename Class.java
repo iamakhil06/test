022fd90a406343925cd2ae6cc6db0256a808cf4d
iamakhil06 <akhil0695@gmail.com>
2020-05-23 11:53:24 +0530

    critical bugs fixed

--- Class.java
+++ Class.java
@@ -1,5 +1,8 @@
 public void Class{
 	public static void main(String[] args){
-		System.out.println("Welcome to the club!")
+		System.out.println("Welcome to the club!");
+	}
+	public class() {
+		System.out.println("new method added");
 	}
 }
